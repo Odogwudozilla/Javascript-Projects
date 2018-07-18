0d594b4cb1ded1b1b29eb5911b6264b9e80a37ca
Chidozie Nnachor <odogwu.naija@gmail.com>
2018-07-18 02:28:05 +0100

    First commit for calculator

---
+++ calculator/calc.js
@@ -0,0 +1,11 @@
+/*
+function myFunction() {
+  var x = document.getElementById("calcform");
+  var text = "";
+  var i;
+
+  text += x.elements[i].value;
+
+  document.getElementById("calcform").innerHTML = text;
+}
+*/
